6d9a129cd8896ce1364606be5c418e08e9fefdc3
power4-MaLe <marvin.leyendecker@power4-its.de>
2025-07-21 17:17:09 +0200

    task 1

--- database/seeders/GolferSeeder.php
+++ database/seeders/GolferSeeder.php
@@ -9,8 +9,18 @@ class GolferSeeder extends Seeder
 {
     public function run(): void
     {
+
+        /**
+         * Task 1:
+         * Erstellt 100 Golfer mit fortlaufenden debitor_account Nummern von 1000 bis 1099
+         * Bei jedem Durchlauf des Seeders werden dieselben debitor_account Nummern vergeben.
+         */
+
         Golfer::factory()
             ->count(100)
+            ->sequence(fn ($sequence) => [
+                'debitor_account' => 1000 + $sequence->index,
+            ])
             ->create();
     }
 }
