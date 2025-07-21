5b070edccfee233e787e944a8ad3d8f4796709b7
power4-MaLe <marvin.leyendecker@power4-its.de>
2025-07-21 17:27:02 +0200

    task 2

--- database/seeders/GolferSeeder.php
+++ database/seeders/GolferSeeder.php
@@ -11,15 +11,18 @@ class GolferSeeder extends Seeder
     {
 
         /**
-         * Task 1:
-         * Erstellt 100 Golfer mit fortlaufenden debitor_account Nummern von 1000 bis 1099
-         * Bei jedem Durchlauf des Seeders werden dieselben debitor_account Nummern vergeben.
+         * Task 2:
+         * Fügt 100 Golfer mit fortlaufenden debitor_account Nummern hinzu
+         * Der aktuell höchste vergebene debitor_account wird abgefragt und der nächste freie Wert wird verwendet.
+         * Ergebnis: keine Duplikate mehr bei mehrfachem Durchlauf des Seeders
          */
+
+        $lastDebitorNumber = Golfer::max('debitor_account') ?? 999;
 
         Golfer::factory()
             ->count(100)
             ->sequence(fn ($sequence) => [
-                'debitor_account' => 1000 + $sequence->index,
+                'debitor_account' => $lastDebitorNumber + 1 + $sequence->index,
             ])
             ->create();
     }
