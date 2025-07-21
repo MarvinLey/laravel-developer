facfb89b5c25fa47e530cc9e9b53ae1cca6b6098
power4-MaLe <marvin.leyendecker@power4-its.de>
2025-07-21 19:16:01 +0200

    task 4

--- app/Http/Controllers/GolferController.php
+++ app/Http/Controllers/GolferController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\GolferResource;
 use App\Models\Golfer;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class GolferController extends Controller
 {
@@ -28,6 +29,52 @@ class GolferController extends Controller
             ->additional([
                 'total_golfers' => $golfers->count(),
             ]);
+    }
+
+    /**
+     *Gibt die 500 nächstgelegenen Golfer als CSV zum Download zurück
+     */
+    public function nearbyCsv(Request $request)
+    {
+        $validated = $request->validate([
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $latitude = $validated['latitude'];
+        $longitude = $validated['longitude'];
+
+        $golfers = Golfer::nearby($latitude, $longitude);
+
+        $headers = [
+            "Content-Type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=golfers_nearby.csv",
+        ];
+
+        $columns = [
+            'id', 'debitor_account', 'name', 'email', 'latitude', 'longitude', 'distance (km)'
+        ];
+
+        $callback = function() use ($golfers, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($golfers as $golfer) {
+                fputcsv($file, [
+                    $golfer->id,
+                    $golfer->debitor_account,
+                    $golfer->name,
+                    $golfer->email,
+                    $golfer->latitude,
+                    $golfer->longitude,
+                    round($golfer->distance, 2),
+                ]);
+            }
+
+            fclose($file);
+        };
+
+        return new StreamedResponse($callback, 200, $headers);
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GolferController;
 
 Route::get('/golfers/nearby', [GolferController::class, 'nearby']);
+Route::get('/golfers/nearby/csv', [GolferController::class, 'nearbyCsv']);
 
